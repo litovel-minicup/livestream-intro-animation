1ceef5fdcd3ef452b70547d229811e323cb87750
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2018-06-02 01:06:40 +0200

    Extend Triangle primitive interface

--- triangleprimitive.cpp
+++ triangleprimitive.cpp
@@ -2,15 +2,31 @@
 
 
 
+TrianglePrimitive::TrianglePrimitive(const QColor &color)
+{
+    m_color = color;
+}
+
 TrianglePrimitive::TrianglePrimitive(float x1, float y1,
                                      float x2, float y2,
                                      float x3, float y3,
                                      const QColor &color)
 {
     m_color = color;
-    m_p1 = QVector3D(x1, y1, 1);
-    m_p1 = QVector3D(x2, y2, 1);
-    m_p1 = QVector3D(x3, y3, 1);
+    m_p[1] = QVector3D(x1, y1, 1);
+    m_p[2] = QVector3D(x2, y2, 1);
+    m_p[3] = QVector3D(x3, y3, 1);
+}
+
+TrianglePrimitive::TrianglePrimitive(const QVector3D &p1,
+                                     const QVector3D &p2,
+                                     const QVector3D &p3,
+                                     const QColor &color)
+{
+    m_color = color;
+    m_p[1] = p1;
+    m_p[2] = p2;
+    m_p[3] = p3;
 }
 
 QColor TrianglePrimitive::color() const
@@ -18,17 +34,38 @@ QColor TrianglePrimitive::color() const
     return m_color;
 }
 
-QVector3D TrianglePrimitive::p1() const
+QVector3D TrianglePrimitive::point(int i) const
 {
-    return m_p1;
+    return m_p[i];
 }
 
-QVector3D TrianglePrimitive::p2() const
+void TrianglePrimitive::setColor(const QColor &col)
 {
-    return m_p2;
+    m_color = col;
 }
 
-QVector3D TrianglePrimitive::p3() const
+void TrianglePrimitive::setPoint(int i, const QVector3D &p)
 {
-    return m_p3;
+    m_p[i] = p;
+}
+
+void TrianglePrimitive::setPoint(int i, float x, float y)
+{
+    m_p[i] = QVector3D { x, y, 1 };
+}
+
+QVariant TrianglePrimitive::toVariant() const
+{
+    QVariantMap data;
+    data["color"] = m_color;
+    data["points"] = QVariantList { m_p[0], m_p[1], m_p[2] };
+
+    return data;
+}
+
+
+QDebug operator <<(QDebug debug, const TrianglePrimitive &t)
+{
+    debug << "Triangle(" << t.point(0)<< ", " << t.point(1) << ", " << t.point(2) << ")";
+    return debug;
 }
--- triangleprimitive.h
+++ triangleprimitive.h
@@ -2,27 +2,38 @@
 #define TRIANGLEPRIMITIVE_H
 
 #include <QColor>
+#include <QDebug>
 #include <QVector3D>
 
 
 class TrianglePrimitive
 {
     private:
-        QVector3D m_p1;
-        QVector3D m_p2;
-        QVector3D m_p3;
+        QVector3D m_p[3];
         QColor m_color;
 
     public:
+        TrianglePrimitive() {}
+        TrianglePrimitive(const QColor& color);
         TrianglePrimitive(float x1, float y1,
                           float x2, float y2,
                           float x3, float y3,
                           const QColor& color);
+        TrianglePrimitive(const QVector3D& p1,
+                          const QVector3D& p2,
+                          const QVector3D& p3,
+                          const QColor& color);
 
         QColor color() const;
-        QVector3D p1() const;
-        QVector3D p2() const;
-        QVector3D p3() const;
+        QVector3D point(int i) const;
+
+        void setColor(const QColor& col);
+        void setPoint(int i, const QVector3D& p);
+        void setPoint(int i, float x, float y);
+
+        QVariant toVariant() const;
 };
+
+QDebug operator <<(QDebug debug, const TrianglePrimitive& t);
 
 #endif // TRIANGLEPRIMITIVE_H
